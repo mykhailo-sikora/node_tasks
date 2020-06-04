cfaceff968675b27c0155adc4d034eb2702cf3a6
mykhailo-sikora <43365970+mykhailo-sikora@users.noreply.github.com>
2020-06-04 13:40:39 +0300

    fixed product, user validators

--- middlewares/product/isProductExist.js
+++ middlewares/product/isProductExist.js
@@ -7,7 +7,7 @@ const {productValidJoiSchema} = require('../../validators');
 module.exports = async (req, res, next) => {
     try {
         const {id} = req.params;
-        if (isNaN(id) || +id < 0) throw new Error('Product is not valid');
+        if (isNaN(id) || +id < 0) throw new errorHandler('Product is not valid', 400, 4001);
 
         next();
     } catch (e) {
---
+++ middlewares/user/isUserExist.js
@@ -0,0 +1,13 @@
+// const Joi = require('joi');
+// const {errorHandler} = require('../../errors');
+//
+// const {userValidJoiSchema} = require('../../validators');
+//
+//
+// module.exports = async (req,res,next) =>{
+//  const {id} = req.params;
+//
+//  if(isNaN(id) || +id <0) return next(new errorHandler('User is not valid'));
+//
+//
+// };
